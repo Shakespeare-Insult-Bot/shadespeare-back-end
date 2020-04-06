584d4d438565a1cc6c922c4d40925ea64a8857c3
Scott Harlan <56773004+SeHarlan@users.noreply.github.com>
2020-04-06 16:19:07 -0700

    Scott (#16)
    * monty stuff
    
    * data stuf
    
    * 'playing'
    
    * scotts fav
    
    * merge
    
    * cleaned up files
    
    * files and markov
    
    Co-authored-by: Fiona Ochs <55855284+fionaochs@users.noreply.github.com>

--- sandbox.js
+++ sandbox.js
@@ -1,57 +1,59 @@
-const MarkovChain = require('markovchain');
+// const MarkovChain = require('markovchain');
+const Markov = require('markov-chains-text').default;
 const chance = require('chance').Chance();
 
 
-const { text: genericText } = require('./lib/data/genericToShakespeare');
+const { text: translatedText } = require('./lib/data/translated-shakespeare');
 const { text: midSummerText } = require('./lib/data/midsummer-nights-dream');
 const { text: montyText } = require('./lib/data/monty-python.js');
 const { text: muchAdoText } = require('./lib/data/much-ado-data');
-const { text: oShakeText } = require('./lib/data/o-shakespear-insults');
-const { text: ourText } = require('./lib/data/our-insults');
+const { text: extraShakeText } = require('./lib/data/extra-shakespeare-insults');
 const { text: shakeText } = require('./lib/data/shakespeare-insults');
 const { text: twelfthText } = require('./lib/data/twelfth-night');
+<<<<<<< HEAD
+=======
 const { text: favorites } = require('./lib/data/favorites');
 
 const { text: scottsFavs } = require('./lib/data/scottsFavs');
 
 const { text: fiFavorites } = require('./lib/data/fi-favorites');
 
+>>>>>>> a769df8f85347d33aba18f320482623905305074
 
-// const endCondition = function(sentence) {
-
-//   if(sentence.split(' ').length >= 30) return;
-// };
+const { text: favorites } = require('./lib/data/ourFavorites');
+const { text: scottsFavs } = require('./lib/data/scottsFavorites');
+const { text: fiFavs } = require('./lib/data/fionasFavorites');
+const { text: codyFavs } = require('./lib/data/codysFavorites');
+const { text: jamesFavs } = require('./lib/data/jamesFavorites');
 
 function makeTwit() {
-  // const quoteChain = new MarkovChain(twelfthText + montyText + montyText + genericText + ourText + shakeText + genericText + midSummerText + muchAdoText + oShakeText); //what we had
+  // const quoteChain = new MarkovChain(montyText + genericText + ourText + shakeText + oShakeText + scottsFavs + favorites + fiFavorites + codyFavs + jamesFavs); //all jokes...pretty good
+  // const startWords = ['I', 'The', 'Thou', 'Thy', 'Thine', 'You', 'Thee', 'My', 'Peace', 'A', 'Listen', 'Here'];
+  // const ending = ['.', '!'];
+  // let quote = '';
+  // while(quote.length > 260 || quote.length < 10 || quote.split(' ').length === 1) {
+  //   quote = quoteChain.start(chance.pickone(startWords)).end(15).process();
+  // }
+  // const endChar = quote[quote.length - 1];
+  // if(endChar === ',' || endChar === '.') quote.slice(quote.length - 1);
+  // return quote + chance.pickone(ending);
 
-  // const quoteChain = new MarkovChain(twelfthText + montyText + montyText + genericText + ourText + shakeText + genericText + midSummerText + muchAdoText + oShakeText); //what we had
-
-
-  const quoteChain = new MarkovChain(montyText + genericText + ourText + shakeText + oShakeText + scottsFavs); //all jokes...pretty good
-
-  // const quoteChain = new MarkovChain(montyText + genericText + ourText + shakeText + oShakeText + muchAdoText); //just jokes and much ado... not great but make more sense
-
-  // const quoteChain = new MarkovChain(montyText + genericText + ourText + shakeText + oShakeText + muchAdoText + montyText + genericText + ourText + shakeText + oShakeText + favorites); //just jokes and much ado... not great but make more sense
-
-  // const quoteChain = new MarkovChain(twelfthText + montyText + montyText + montyText + genericText + ourText + shakeText + genericText + midSummerText + muchAdoText + oShakeText + shakeText + shakeText + genericText + oShakeText + shakeText + ourText); //all but weighted jokes...kinda bad...too much
-
-  // const quoteChain = new MarkovChain(twelfthText + montyText + montyText + genericText + ourText + shakeText + genericText + midSummerText + muchAdoText + oShakeText);
-  // const quoteChain = new MarkovChain(twelfthText + montyText + montyText + genericText + ourText + shakeText + genericText + midSummerText + muchAdoText + oShakeText);
-  const startWords = ['I', 'The', 'Thou', 'Thy', 'Thine', 'You', 'Thee'];
-  // const startWords = ['Thou art'];
-  // const startWords = muchAdoText.split(' ');
-  const ending = ['.', '!'];
-  let quote = '';
-  
-  while(quote.length > 260 || quote.length < 10 || quote.split(' ').length === 1) {
-    quote = quoteChain.start(chance.pickone(startWords)).end(15).process();
+  ///////////
+  const markovchain = new Markov(montyText + translatedText + shakeText + extraShakeText + scottsFavs + favorites + fiFavs + codyFavs + jamesFavs + muchAdoText);
+  let quote = markovchain.makeSentence({ tries: 10 });
+  while(quote.length > 260 || quote.length < 10) {
+    quote = markovchain.makeSentence({ tries: 10 });
   }
 
-  const endChar = quote[quote.length - 1];
-  if(endChar === ',' || endChar === '.') quote.slice(quote.length - 1);
+  quote = quote.trim();
 
-  return quote + chance.pickone(ending);
+  if(quote.startsWith('and ')) {
+    quote = quote.slice(4);
+  }
+  const firstLetter = quote[0].toUpperCase();
+  const capQuote = firstLetter + quote.slice(1);
+  return capQuote;
+  //////
 }
 
-console.log('==========tweet: ', makeTwit());
+console.log('==========tweet:', makeTwit());
