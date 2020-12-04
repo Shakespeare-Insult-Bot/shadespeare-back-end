655d4679ab658595dbb05c13d5aeaf902e8b7ca2
Scott <seharlan@gmail.com>
2020-12-03 18:20:52 -0800

    fire fix, comment out mongodb stuff and extras

--- cron-practice.js
+++ cron-practice.js
@@ -10,21 +10,21 @@
 // }, null, true, 'America/Los_Angeles');
 // job.start();
 
-const CronJob = require('cron').CronJob;
-const { postTweet } = require('./lib/utils/twit');
-const makeTwit = require('./lib/utils/markov');
+// const CronJob = require('cron').CronJob;
+// const { postTweet } = require('./lib/utils/twit');
+// const makeTwit = require('./lib/utils/markov');
 
 
-const job = new CronJob('10 * * * * *', function() {
-  const tweet = {
-    quote: makeTwit()
-  };
-  try {
-    postTweet(tweet);
-  } catch(err) {
-    console.log('error posting cron tweet', err);
-  }
-  
-  console.log('Post tweet every 10 seconds');
-}, null, true, 'America/Los_Angeles');
-job.start();
+// const job = new CronJob('10 * * * * *', function() {
+//   const tweet = {
+//     quote: makeTwit()
+//   };
+//   try {
+//     postTweet(tweet);
+//   } catch(err) {
+//     console.log('error posting cron tweet', err);
+//   }
+
+//   console.log('Post tweet every 10 seconds');
+// }, null, true, 'America/Los_Angeles');
+// job.start();
